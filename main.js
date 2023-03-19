d0f976b8daa113d63561ee5ee5e652ab56462e71
nkudssi <90203207+nkudssi@users.noreply.github.com>
2023-03-19 12:32:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ function preload(){
 }
 
 function setup(){
-canvas = createCanvas(400,400)
+    video = createCapture(VIDEO);
+    video.size(550,550);
+    canvas = createCanvas(650,550);
+    canvas.position(660,150);
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on("pose", gotposes)
+
 }
 
 function draw(){
+    background("rgb(192, 192, 255)");
+    fill("purple");
+    textSize(100);
+    text("Nour",100,100)
+}
 
+function modelLoaded() {
+    console.log("poseNet is loaded");
+}
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
